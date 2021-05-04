cdd479aeff17e0c2b082c337559e4a93b4b40604
Karl Frick <karl.frick5@gmail.com>
2021-05-04 14:05:33 -0400

    Getting 10 random jokes from API and displaying them in a table using ConsoleTable

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ConsoleTables;
 
 namespace ApiClient
 {
+    class Item
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string setup { get; set; }
+        public string punchline { get; set; }
+    }
     class Program
     {
         static async Task Main(string[] args)
         {
             var client = new HttpClient();
 
-            var responseAsString = await client.GetStringAsync("https://official-joke-api.appspot.com/random_joke");
+            var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/jokes/programming/ten");
 
-            Console.WriteLine(responseAsString);
+            var jokes = await JsonSerializer.DeserializeAsync<List<Item>>(responseAsStream);
+
+            var table = new ConsoleTable("Type", "Setup", "Punchline");
+
+            foreach (var joke in jokes)
+            {
+                table.AddRow(joke.type, joke.setup, joke.punchline);
+            }
+            table.Write();
         }
     }
 }
